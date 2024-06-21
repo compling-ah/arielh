e3780282f257efe60d4f1eb1e095194a7fca5edc
arielhsia <irargb@gmail.com>
2024-06-21 20:33:54 +0800

    modify

--- 20224_06_10/index.py
+++
@@ -1,6 +0,0 @@
-
-
-import tools
-
-tools.menu(wine='白酒',entree='牛排',dessert='蛋糕')
-print(tools.PI)
--- 20224_06_10/tools.py
+++
@@ -1,9 +0,0 @@
-#常數
-#class
-#function
-PI = 3.1415926
-
-def menu(wine:str, entree:str, dessert:str) -> None:
-    print(f"wine:{wine}")
-    print(f"entree:{entree}")
-    print(f"dessert:{dessert}")
